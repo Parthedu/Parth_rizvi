6ded4a1ed93c24eeabf1a82e80e8976563f725f4
KAMBLE PARTH GANESH <parthkamble2@eng.rizvi.edu.in>
2024-11-25 10:59:35 +0530

    Update Exp 1.1.c

--- Experiment 1/Exp 1.1.c
+++ Experiment 1/Exp 1.1.c
@@ -1,9 +1,3 @@
-Expt 1 - Task 1
-    Module 1 : Fundamentals of C Programming
-    Aim: WAP to print your name, age, class, division  
-        and UIN on the screen. Use escape sequences 
-        to properly format the output.
-
 #include<stdio.h>
 int main(){
     printf("Full Name\t: Sanjay Singh\n");
