eab382795298fcfa4a2a03934a1c66065ad00995
KAMBLE PARTH GANESH <parthkamble2@eng.rizvi.edu.in>
2024-12-11 10:17:30 +0530

    Create task .c

---
+++ Experiment 8 /task .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+
+  char str[100];
+  int length = 0;
+
+  printf("\t\t** String Length Finder **\n\n");
+
+  printf("Enter Your String:");
+  gets(str);
+
+  while(str[length] != '\0'){
+     length++;
+  }
+
+   printf("String Length: %d Characters", length);
+
+   return 0;
+}
